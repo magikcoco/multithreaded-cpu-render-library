cf75d617d199daadff93bf306b9024009311e8d7
magikcoco <magikcoco.dev@gmail.com>
2024-02-29 16:58:37 -0500

    improved commenting

--- include/nagato.h
+++ include/nagato.h
@@ -1,6 +1,7 @@
 #ifndef NAGATO_H
 #define NAGATO_H
 
+//master header file
 #include "key_constants.h"
 #include "windowing.h"
 #include "compositing.h"
--- include/scaling.h
+++ include/scaling.h
@@ -3,10 +3,18 @@
 
 #include "png_image.h"
 
-// Function to scale an image using nearest neighbor algorithm
+/*
+ * scales an image to a new width and height using nearest neighbor scaling
+ * returns a newly created PNG_Image struct
+ * does not deallocate the original PNG_Image at all
+ */
 PNG_Image* nearest_neighbor_scale(PNG_Image* orig, int new_width, int new_height);
 
-// Function to scale an image using bilinear interpolation algorithm
+/*
+ * scales an image to a new width and height using nearest bilinear interpolation scaling
+ * returns a newly created PNG_Image struct
+ * does not deallocate the original PNG_Image at all
+ */
 PNG_Image* bilinear_interpolation_scale(PNG_Image* orig, int new_width, int new_height);
 
 #endif // IMAGE_SCALING_H
