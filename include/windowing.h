00c7c171c7631e2c67766bbea4ebc55755bb0db7
magikcoco <magikcoco.dev@gmail.com>
2024-02-29 16:53:44 -0500

    improved commenting

--- include/windowing.h
+++ include/windowing.h
@@ -10,15 +10,54 @@ typedef void (*KeyHandler)(void);
 typedef unsigned int KeyMap;
 
 // Function declaration
+/*
+ * raises a signal for termination request
+ */
 void shutdown();
+
+/*
+ * updates the image displayed in the window using a filepath
+ */
 void update_image_from_file(char *filepath);
+
+/*
+ * returns true if nearest neighbor scaling is in use, false otherwise
+ */
 bool get_scaling_nn();
+
+/*
+ * returns true if bilinear interpolation scaling is in use, false otherwise
+ */
 bool get_scaling_bli();
+
+/*
+ * sets the scaling algorithm to nearest neighbor scaling
+ */
 void set_scaling_nn();
+
+/*
+ * sets the scaling algorithm to nearest bilinear interpolation
+ */
 void set_scaling_bli();
+
+/*
+ * adds a key event handler
+ */
 void handle_key_event(KeyHandler handler, KeyMap key);
+
+/*
+ * removes the event handler for the key specified
+ */
 void remove_key_handler(KeyMap key);
+
+/*
+ * sets the window parameters
+ */
 void set_window_parameters(int set_x, int set_y, int set_width, int set_height, int set_border_width);
+
+/*
+ * starts the window loop, with a function signature acceptable for pthread
+ */
 void start_gui();
 
 #endif // WINDOWING_H
