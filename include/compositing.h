4ef3861c0ce7126b1034111b1a81cf5532f22bc2
magikcoco <magikcoco.dev@gmail.com>
2024-02-29 16:50:41 -0500

    improved commenting

--- include/compositing.h
+++ include/compositing.h
@@ -7,6 +7,14 @@ typedef struct {
     png_byte red, green, blue, alpha;
 } rgba_color;
 
+//TODO: function to queue up images to be flattened into a single image
+//TODO: function to flatten all queued iamges into a single image
+
+/*
+ * Blends an image with a specified background color by modifying the image's pixel data in place
+ * Assumes pixels are represented as four consecutive bytes (RGBA: Red, Green, Blue, Alpha) in a flat array
+ * Sets the opacity to full for every pixel
+ */
 void blend_with_background(png_bytep img_data, int width, int height, rgba_color background_color);
 
 #endif
