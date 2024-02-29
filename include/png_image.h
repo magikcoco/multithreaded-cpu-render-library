25700ae295fda3d9c520a1ba1b661e64032d9303
magikcoco <magikcoco.dev@gmail.com>
2024-02-29 11:57:29 -0500

    changed destroypngimage function signature so that you dont have to worry about hanging pointers afterwards

--- include/png_image.h
+++ include/png_image.h
@@ -15,6 +15,6 @@ typedef struct PNG_Image {
 PNG_Image* png_load_from_memory(unsigned char* memory, size_t memory_size);
 PNG_Image* png_load_from_file(char *filepath);
 PNG_Image* CreatePNG_Image(int width, int height, int bitDepth, int colorType);
-void DestroyPNG_Image(PNG_Image* img);
+void DestroyPNG_Image(PNG_Image** imgPtr);
 
 #endif // SIMPLE_IMAGE_H
