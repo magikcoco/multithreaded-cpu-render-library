652dec9bdc5c099de7993bfc6a1365bebccd1e7a
magikcoco <magikcoco.dev@gmail.com>
2024-03-06 16:38:41 -0500

    reorganized windowing.h

--- include/windowing.h
+++ include/windowing.h
@@ -11,71 +11,32 @@ typedef void (*KeyHandler)(void);
 // Define KeyCode as an alias for unsigned int
 typedef unsigned int KeyMap;
 
-/*
- * raises a signal for termination request
- */
+// Shutdown
 void shutdown();
-
-/*
- * checks if the shutdown flag has been triggered
- */
 bool is_gui_shutdown();
 
-/*
- * updates the image displayed in the window using an existing PNG_Image in memory
- */
-void update_image_from_memory(PNG_Image* newImage);
-
-/*
- * updates the image displayed in the window using a filepath
- */
-void update_image_from_file(char *filepath);
-
-/*
- * returns true if nearest neighbor scaling is in use, false otherwise
- */
-bool get_scaling_nn();
-
-/*
- * returns true if bilinear interpolation scaling is in use, false otherwise
- */
-bool get_scaling_bli();
-
-/*
- * sets the scaling algorithm to nearest neighbor scaling
- */
-void set_scaling_nn();
-
-/*
- * sets the scaling algorithm to nearest bilinear interpolation
- */
-void set_scaling_bli();
-
-/*
- * adds a key event handler
- */
-void handle_key_event(KeyHandler handler, KeySym key);
-
-/*
- * removes the event handler for the key specified
- */
-void remove_key_handler(KeySym key);
-
-/*
- * gets mouse position
- */
-int get_mouse_position(int *x, int *y);
-
-/*
- * sets the window parameters
- */
+// Window configuration
 void set_window_parameters(int set_x, int set_y, int set_width, int set_height, int set_border_width);
 
+// Image update functions
+void update_image_from_memory(PNG_Image* newImage);
+void update_image_from_file(char *filepath);
+
+// Scaling algorithms
+bool get_scaling_nn();
+bool get_scaling_bli();
+void set_scaling_nn();
+void set_scaling_bli();
+
+// Event handling and input query
+void handle_key_event(KeyHandler handler, KeySym key);
+void remove_key_handler(KeySym key);
+int get_mouse_position(int *x, int *y);
+
+// Frame rate control
 void frame_rate_control(int targetFPS);
 
-/*
- * starts the window loop, with a function signature acceptable for pthread
- */
+// Start window loop
 void start_gui();
 
 #endif // WINDOWING_H
