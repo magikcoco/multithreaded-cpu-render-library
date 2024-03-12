f955f189f8929c7647cc08213630c694ccb2e33c
magikcoco <magikcoco.dev@gmail.com>
2024-03-12 04:28:14 -0400

    Updated master header file

--- include/nagato.h
+++ include/nagato.h
@@ -1,12 +1,13 @@
 #ifndef NAGATO_H
 #define NAGATO_H
 
-//master header file
+// Master header file
 #include "compositing.h"
 #include "key_constants.h"
 #include "logo.h"
 #include "png_image.h"
 #include "scaling.h"
+#include "timing.h"
 #include "windowing.h"
 
 #endif
