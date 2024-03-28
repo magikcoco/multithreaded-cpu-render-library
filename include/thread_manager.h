75546056363d60e39d4e3c60b0d5c8bacfa03430
magikcoco <magikcoco.dev@gmail.com>
2024-03-27 23:17:18 -0400

    updated thread manager header file

--- include/thread_manager.h
+++ include/thread_manager.h
@@ -10,9 +10,24 @@ typedef struct PoolTask {
     void *arg;
 } PoolTask;
 
+/*
+ * Submit a task to the thread pool for completion
+ */
 TaskID submit_task(PoolTask* task);
+
+/*
+ * Wait for the task identified by the given ID to complete
+ */
 void wait_for_task_completion(TaskID id);
+
+/*
+ * Trigger application shutdown. Worker threads will all stop wworking and return.
+ */
 void signal_shutdown();
+
+/*
+ * True if application shutdown has triggered, false otherwise
+ */
 bool is_application_shutdown();
 
 #endif // THREAD_POOLING_H
